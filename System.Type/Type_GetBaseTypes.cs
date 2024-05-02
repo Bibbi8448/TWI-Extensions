ef3de2e3728c99751bd34bd20c69bc728d699ecd
Tyler Wiebe <bibbi8448@gmail.com>
2024-05-02 12:39:57 -0500

    Create Type_GetBaseTypes.cs

---
+++ System.Type/Type_GetBaseTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace TWI.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class Type_GetBaseTypes
+    {
+        public static IEnumerable<Type> GetBaseTypes(this Type? type)
+        {
+            while (type != null)
+            {
+                yield return type;
+                type = type.BaseType;
+            }
+        }
+    }
+}
